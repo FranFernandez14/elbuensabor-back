3474d590c609e7cc9f737041272e6a19bbc8788b
SebaOgas <saoga07.sao@gmail.com>
2023-11-11 11:30:28 -0300

    SecurityConfig creado
    No testeado

---
+++ src/main/java/com/utn/elbuensabor/Config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.utn.elbuensabor.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import com.utn.elbuensabor.Jwt.JwtAuthenticationFilter;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf ->
+                    csrf
+                            .disable())
+            .authorizeHttpRequests(authRequest ->
+                authRequest
+                        //El uso de AntPathRequestMatcher fue indicado por el profe el 10/11 a las 20:59 por el grupo de Whatsapp
+                        //Autenticacion
+                        .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
+                        //Consola H2:
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        //Autorizacion de acceso a las urls:
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/facturacion/**")).hasAnyAuthority("ADMINISTRADOR", "DELIVERY", "CAJERO", "CLIENTE")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/pedidos/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO", "DELIVERY", "CAJERO", "CLIENTE")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/producto/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO", "DELIVERY", "CAJERO", "CLIENTE")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/rubroProducto/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO", "DELIVERY", "CAJERO", "CLIENTE")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/compraInsumo/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/detalleCompra/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/receta/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/detalleReceta/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/insumo/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/rubroInsumo/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/productos/unidadMedida/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO")
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/usuarios/**")).hasAnyAuthority("ADMINISTRADOR", "COCINERO", "DELIVERY", "CAJERO", "CLIENTE")
+
+            )
+            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) //H2
+            .sessionManagement(sessionManager->
+                    sessionManager
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+
+
+}

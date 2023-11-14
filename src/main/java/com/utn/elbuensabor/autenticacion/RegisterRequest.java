6a5e5e8aee1dc6887d49606d981f1cb4677602cb
MartinaCM21 <69211616+MartinaCM21@users.noreply.github.com>
2023-11-13 23:09:26 -0300

    classes

--- src/main/java/com/utn/elbuensabor/autenticacion/RegisterRequest.java
+++ src/main/java/com/utn/elbuensabor/autenticacion/RegisterRequest.java
@@ -1,4 +1,22 @@
 package com.utn.elbuensabor.autenticacion;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String phoneNumber;
+    String email;
+    String password;
+
 }
